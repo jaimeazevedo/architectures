8b49c884e074ad50dbc1706207a71c51fb21c86d
Jaime Paulo <jaime.azevedo.88@gmail.com>
2020-09-22 18:04:09 +0100

    Aded MVVMC project for that architecture implementation.

---
+++ MVVMC/MVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Jaime Azevedo on 22/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
